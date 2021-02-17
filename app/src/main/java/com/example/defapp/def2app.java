eaee103911ee68fc04e7c287d9f0fd45bda3a644
Madaffadl26 <@m.daffafadhil26@gmail.com>
2021-02-17 23:06:32 +0700

    daffa

--- app/src/main/java/com/example/defapp/def2app.java
+++ app/src/main/java/com/example/defapp/def2app.java
@@ -3,14 +3,42 @@ package com.example.defapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class def2app extends AppCompatActivity {
+
+    public Spinner sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_def2app);
 
+        sp = (Spinner) findViewById(R.id.spinner);
+
+        // untuk membuat list tema pelatihan
+        List<String> item = new ArrayList<>();
+        item.add("Networking Academy");
+        item.add("Front-end Developer");
+        item.add("Back-end Developer");
+        item.add("Artificial Intelegence");
+        item.add("IT Essentials");
+        item.add("Android Developer");
+        item.add("Cyber Security");
+        item.add("UI/UX Design");
+
+        // Untuk membuat adapter listnya
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(def2app.this, android.R.layout.simple_spinner_dropdown_item,item);
+
+        // untuk menentukan model adapternya
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        sp.setAdapter(adapter);
 
     }
 }
