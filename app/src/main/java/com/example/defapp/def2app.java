7c59a4ad21a67588546afdfa1aff85d4fe1bf3e0
Madaffadl26 <@m.daffafadhil26@gmail.com>
2021-02-17 10:24:16 +0700

    M.Daffa Fadillah

---
+++ app/src/main/java/com/example/defapp/def2app.java
@@ -0,0 +1,16 @@
+package com.example.defapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class def2app extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_def2app);
+
+
+    }
+}
